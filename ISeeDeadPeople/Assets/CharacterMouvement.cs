79c73cb13841d41abfe270cd206c2ea572e2831e
Karpys <remi.vedrenne.pro@gmail.com>
2021-09-07 16:33:56 +0200

    [Feature] Wall System

--- ISeeDeadPeople/Assets/CharacterMouvement.cs
+++ ISeeDeadPeople/Assets/CharacterMouvement.cs
@@ -6,6 +6,8 @@ public class CharacterMouvement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float Speed;
+    public float SpeedInWall;
+    public bool InWall;
     public CharacterController Controller;
 
     void Start()
@@ -16,11 +18,14 @@ public class CharacterMouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!InWall)
+        {
         float Axex = Input.GetAxis("Horizontal");
         float Axey = Input.GetAxis("Vertical");
 
         Vector3 Axes = transform.right * Axex + transform.up * Axey;
         Axes = Axes * Speed * Time.deltaTime;
         Controller.Move(Axes);
+        }
     }
 }
