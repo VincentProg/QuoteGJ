f16b90c9c1892c15a161907ac85eb430c07fbbd6
Karpys <remi.vedrenne.pro@gmail.com>
2021-09-07 15:39:59 +0200

    [Feature] Basic Character Mouvement

---
+++ ISeeDeadPeople/Assets/CharacterMouvement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMouvement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Speed;
+    public CharacterController Controller;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Axex = Input.GetAxis("Horizontal");
+        float Axey = Input.GetAxis("Vertical");
+
+        Vector3 Axes = transform.right * Axex + transform.up * Axey;
+        Axes = Axes * Speed * Time.deltaTime;
+        Controller.Move(Axes);
+    }
+}
