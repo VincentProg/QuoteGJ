dc2f6b7fa67243b1279da41f5d499da82a85c597
Romain Nebrazif Habib <romain0habib@gmail.com>
2021-09-10 00:09:22 +0200

    Modifs to Character Mat in Proto Modifs to Breath Display

--- ISeeDeadPeople/Assets/Scripts/Gameplay/QTE/BreathQTE.cs
+++ ISeeDeadPeople/Assets/Scripts/Gameplay/QTE/BreathQTE.cs
@@ -34,7 +34,7 @@ public class BreathQTE : QTE
 
         displaySetup = currentDisplayQTE.GetComponent<QTEDisplay>();
         QTEManager.instance.AssignSprite(Button, displaySetup);
-        displaySetup.transform.position = GameManager.Instance.player.transform.position + new Vector3(0,0,20);
+        displaySetup.transform.position = GameManager.Instance.player.transform.position + new Vector3(0,0, 4.5f);
 
         hasBeenLaunched = true;
     }
